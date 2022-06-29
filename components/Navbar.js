154a8b0850ca6da04efb2a5ced7b0262c1479574
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-06-29 22:27:22 +0800

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar () {
+  return (
+    <nav className="py-12 flex justify-between bg-transparent">
+      <div className="flex flex-row justify-center items-center">
+        <img src="/logo.png" className="h-8 w-auto" />
+        <h1 className="px-2 text-3xl text-white display text-center my-auto font-bold sm:hidden">DabIcons</h1>
+      </div>
+      <div className="flex flex-row justify-center items-center my-auto">
+        <a href="/" className="text-white text-lg px-2">
+          Docs
+        </a>
+        <a href="/" className="ml-2 text-lg px-6 py-2 bg-green-600 text-white hover:bg-green-700 rounded-full transition active:ring">
+          Github
+        </a>
+      </div>
+    </nav>
+  )
+}
