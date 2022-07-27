ed56b8ae0edc31085cec5bef288b94bdce2a751e
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-07-27 10:08:20 +0800

    Update Pricing.js

--- components/landing/Pricing.js
+++ components/landing/Pricing.js
@@ -10,7 +10,7 @@ export default function Pricing () {
           <span className="ml-3 text-base font-medium tracking-tight text">Pricing</span>
         </h2>
         <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">Pick your package</p>
-        <p className="mt-4 max-w-xl text-lg tracking-tight text-slate-600">DabIcons is available in two different packages so you can pick the one that's right for you.</p>
+        <p className="mt-4 max-w-xl text-lg tracking-tight text-slate-600">DabIcons is available in two different packages so you can pick the one that&apos;s right for you.</p>
       </div>
       <div className="mx-auto mt-16 max-w-5xl lg:px-6">
         <div className="grid bg-slate-50 sm:px-6 sm:pb-16 md:grid-cols-2 md:rounded-6xl md:px-8 md:pt-16 lg:p-20">
